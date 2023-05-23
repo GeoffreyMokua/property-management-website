84edd784f80a0ad2d36e5f87300fc6ab3bd4b840
geoffrey <arisimokua280@gmail.com>
2023-05-24 00:01:45 +0300

    added services section to home page

--- src/App/Home/index.jsx
+++ src/App/Home/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import Banner from '../../components/Banner'
+import Service from '../../components/Service'
 const Home = () => {
   return (
     <div>
          <Header />
          <Banner />
+         <Service />
         <Footer />
     </div>
   )
