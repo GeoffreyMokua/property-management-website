b0e7f1f1ff68af76e60e66b08950d5bf5bb059da
geoffrey <arisimokua280@gmail.com>
2023-05-27 22:27:47 +0300

    added partnership services to home page

--- src/App/Home/index.jsx
+++ src/App/Home/index.jsx
@@ -3,12 +3,14 @@ import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import Banner from '../../components/Banner'
 import Service from '../../components/Service'
+import Partner from '../../components/Partner'
 const Home = () => {
   return (
     <div>
          <Header />
          <Banner />
          <Service />
+         <Partner />
         <Footer />
     </div>
   )
