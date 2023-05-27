59073ba0ef0d47a7a2fc086ae9b76d3666b1f689
geoffrey <arisimokua280@gmail.com>
2023-05-27 21:03:26 +0300

    added contact features

--- src/App/Contact/index.jsx
+++ src/App/Contact/index.jsx
@@ -6,44 +6,36 @@ const Contact = () => {
   return (
     <>
     <Header />
-    
-    
-<section class="mb-4">
 
-   
-    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-    
-    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+    <section  class="mb-4">
+            <p class="text-center w-responsive mx-auto mb-5">Tired Of Self-Management? Schedule a Consult Today.</p>
+                <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+
+                 <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
         a matter of hours to help you.</p>
 
-    <div class="row">
-
-        
-        <div class="col-md-9 mb-md-0 mb-5">
-            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
-
-               
-                <div class="row">
-
-                   
+        <div class="row">
+            <div class="col-md-9 mb-md-0 mb-5">
+                <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+                  <div class= "row">
                     <div class="col-md-6">
                         <div class="md-form mb-0">
                             <input type="text" id="name" name="name" class="form-control"></input>
-                            <label for="name" class="">Your name</label>
+                             <label for="name" class="">Your name</label>
+
                         </div>
                     </div>
-             
+
                     <div class="col-md-6">
                         <div class="md-form mb-0">
-                            <input type="text" id="email" name="email" class="form-control"></input>
+                            <input type="text" id="email" name="email" class="form-control" ></input>
                             <label for="email" class="">Your email</label>
                         </div>
                     </div>
-                  
 
-                </div>
-               
-                <div class="row">
+                  </div>
+
+                   <div class="row">
                     <div class="col-md-12">
                         <div class="md-form mb-0">
                             <input type="text" id="subject" name="subject" class="form-control"></input>
@@ -51,9 +43,7 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
-              
 
-                
                 <div class="row">
 
                    
@@ -66,36 +56,27 @@ const Contact = () => {
 
                     </div>
                 </div>
-               
 
-            </form>
+                </form>
 
-            <div class="text-center text-md-left">
-                <a class="btn btn-primary">Send</a>
+                <div class="text-center text-md-left">
+                     <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+
+                </div>
+
+                 <div class="status">
+        </div>
+
+         
+
+
             </div>
-            <div class="status"></div>
+
         </div>
-        
-        <div class="col-md-3 text-center">
-            <ul class="list-unstyled mb-0">
-                <li><i class="fas fa-map-marker-alt fa-2x"></i>
-                    <p>Nairobi, Kenya</p>
-                </li>
-
-                <li><i class="fas fa-phone mt-4 fa-2x"></i>
-                    <p>+ 254 724002308</p>
-                </li>
-
-                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
-                    <p>info@goldenmanagement.com</p>
-                </li>
-            </ul>
-        </div>
-       
-
-    </div>
-
-</section>
+            </section>
+            
+    
+   
 <Footer />
 </>
   )
