2deb5b9541a94915466edc63c44ea9763eb8af02
geoffrey <arisimokua280@gmail.com>
2023-05-28 09:02:54 +0300

    added color adjust

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'mdbootstrap/css/mdb.min.css';
 // import 'mdbootstrap/css/bootstrap.min.css';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+// import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
