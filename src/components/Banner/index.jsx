ec0ecc69f5d4cfb74bbf2caa4287f0cb21affb6b
geoffrey <arisimokua280@gmail.com>
2023-06-12 17:31:40 +0300

    changed the banner section

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,45 +1,23 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap';
+import './Banner.css'
+// import { Carousel } from 'react-bootstrap';
 
 
 function Banner() {
   return (
-     <Carousel  variant="dark" >
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://thumbs.dreamstime.com/b/apartment-building-modern-buildings-new-westminster-british-columbia-canada-40351928.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h5>Welcome To Golden Property Management</h5> 
-      
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://img.freepik.com/free-photo/analog-landscape-city-with-buildings_23-2149661462.jpg"
-      alt="Second slide"
-    />
-    <Carousel.Caption>
-      <h5>We Care Our Tenants</h5>
-      
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://img.freepik.com/free-photo/building_1127-3497.jpg "
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-       <h5 className="text-white">Always Near on Your Service</h5>
-      
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-  )
+    <section class="showcase">
+      <img
+        src="https://img.freepik.com/free-photo/modern-villa-with-multiple-lights-sun-loungers-asprovalta-greece_1268-16093.jpg"
+        alt="P"
+      />
+      <div class="overlay">
+        <h2>Golden Property Management</h2>
+        <p>
+          Improving the Lives of Real Estate Investors and Residents Since 2022
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default Banner
