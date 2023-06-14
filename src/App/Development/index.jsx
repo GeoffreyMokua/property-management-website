70a970601a3032d4f1d8803d3da84126650692e4
geoffrey <arisimokua280@gmail.com>
2023-06-14 14:31:13 +0300

    styling the dev page

--- src/App/Development/index.jsx
+++ src/App/Development/index.jsx
@@ -1,30 +1,30 @@
-// import React from "react";
-// import "./Development.css";
-// import Footer from "../../components/Footer";
-// import Header from "../../components/Header";
+import React from "react";
+import "./Development.css";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 
-// function Development() {
-//   return (
-//     <>
-//       <Header />
-//       <section class="show">
-//         <img
-//           src="https://img.freepik.com/free-photo/modern-luxury-domestic-room-comfortable-relaxation-generative-ai_188544-12679.jpg"
-//           alt="P"
-//         />
-//         <div class="overlay">
-//           <h2>
-//             <strong>
-//               OUR <br /> DEVELOPMENTS
-//             </strong>
-//           </h2>
-//           <p>Explore our Luxury Developments.</p>
-//         </div>
-//       </section>
-//       <Footer />
-//     </>
-//   );
-// }
+function Development() {
+  return (
+    <>
+      <Header />
+      <section class="show">
+        <img
+          src="https://img.freepik.com/free-photo/modern-luxury-domestic-room-comfortable-relaxation-generative-ai_188544-12679.jpg"
+          alt="P"
+        />
+        <div class="overlay">
+          <h2>
+            <strong>
+              OUR <br /> DEVELOPMENTS
+            </strong>
+          </h2>
+          <p>Explore our Luxury Developments.</p>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
 
 
-// export default Development;
+export default Development;
